e636ae20ec61066b6d036b3edb35a5c9e233ffd8
amlane <twelvexstring@gmail.com>
2019-05-20 15:57:17 -0500

    updated background color, added CSS reset and put final touches on nav bar before begining create account page

--- fotograph-app/src/components/Navigation/NavBar.js
+++ fotograph-app/src/components/Navigation/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
+import './NavBar.scss';
 import logo from '../../styles/LOGO.png';
 
 function NavBar(){
     return (
         <nav className="navigation-bar">
-            <img src={logo} />
+            <img src={logo} className="logo" />
             <form className="search-bar">
             <button className="search-btn">+</button>
             <input 
@@ -17,6 +18,10 @@ function NavBar(){
             <Link className="nav-links" to="/">Photo Collections</Link>{' '}
             <Link className="nav-links" to="/">Artists</Link>{' '}
             <Link className="nav-links" to="/">About</Link>{' '}
+            <div className="log-link-container">
+            <Link className="log-links" to="/">Log In</Link>{' '}
+            <Link className="log-links" to="/">Log Out</Link>{' '}
+            </div>
         </nav>
     )
 }
