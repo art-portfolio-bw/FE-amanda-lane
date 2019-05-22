0f11f6deeab2d34fd5296bd3d0b28fa7aa3db525
amlane <twelvexstring@gmail.com>
2019-05-22 12:49:06 -0500

    trying fix for react router with netlify

--- fotograph-app/src/components/Navigation/NavBar.js
+++ fotograph-app/src/components/Navigation/NavBar.js
@@ -24,7 +24,7 @@ function NavBar(props){
             <NavLink className="nav-links" exact to="/">Home</NavLink>{' '}
             {props.loggedIn && <NavLink className="nav-links" to="/user"> My Posts</NavLink>}
             <NavLink className="nav-links" to="/artists">Artists</NavLink>{' '}
-            <NavLink className="nav-links" to="/about">About</NavLink>{' '}
+            {/* <NavLink className="nav-links" to="/about">About</NavLink>{' '} */}
             <div className="log-link-container">
             {props.loggedIn ? (
                 <Link className="log-links" to="/" onClick={props.logout}>Log Out</Link>
